d9c1cceaf7123affab5c456246b1486081b18f3d
Eugene Khodosov <nnagatoo@mail.ru>
2019-11-17 00:46:00 +0300

    add Stomp lib

--- app/src/main/java/com/example/findmeclient/service/ConnectionServiceImpl.kt
+++ app/src/main/java/com/example/findmeclient/service/ConnectionServiceImpl.kt
@@ -1,21 +1,25 @@
 package com.example.findmeclient.service
 
 import android.widget.TextView
-import com.example.findmeclient.listener.MyWebSocketListener
-import okhttp3.*
-import java.io.IOException
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.StompClient
 
 class ConnectionServiceImpl(val field: TextView) : ConnectionService {
 
-    private var client = OkHttpClient()
+//    private var client = OkHttpClient()
+    private lateinit var client: StompClient
 
     override fun connect() {
-        val request = Request.Builder()
-            .url("ws://<ip_addr>:8551/radar")
-            .build()
+//        val request = Request.Builder()
+//            .url("ws://10.255.255.9:8551/radar")
+//            .build()
+//
+//        val socket = client.newWebSocket(request, MyWebSocketListener(field))
+//        socket.send("test")
 
-        val socket = client.newWebSocket(request, MyWebSocketListener(field))
-        socket.send("test")
+        client = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://10.255.255.9:8551/radar")
+        client.connect()
+//        client.topic("/topic/greetings")
     }
 
     override fun sendCoordinates(x: Double, y: Double) {
